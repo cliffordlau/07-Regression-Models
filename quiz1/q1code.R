baef5a7e378b9483e04e1176fcbed7e595eaedb6
clau <cliffordlau000@gmail.com>
2015-07-12 11:27:56 +0800

    q8

--- quiz1/q1code.R
+++ quiz1/q1code.R
@@ -20,3 +20,18 @@ m <- with(data, lm(mpg ~ wt))
 
 # Q4
 # beta_1 = corr(x,y) * sd_y / sd_x
+
+# Q5
+# cov(x,y) is the slope for these 2 since they are both normalized
+
+# Q6
+x <- c(8.58, 10.46, 9.01, 9.64, 8.86)
+(x - mean(x))/sd(x)
+
+# @7
+x <- c(0.8, 0.47, 0.51, 0.73, 0.36, 0.58, 0.57, 0.85, 0.44, 0.42)
+y <- c(1.39, 0.72, 1.55, 0.48, 1.19, -1.59, 1.23, -0.65, 1.49, 0.05)
+
+lm(y ~ x)
+
+# Q9
