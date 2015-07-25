98e2dad54ee503b5f21344b0dd0f5e1bf2d9c741
clau <cliffordlau000@gmail.com>
2015-07-25 22:33:22 +0900

    messed up the submission... 4 still wrong

--- quiz3/q3.R
+++ quiz3/q3.R
@@ -50,6 +50,7 @@ anova(model2, model3)
 # may not be necessary.
 
 # this is right
+# But still don't know why i got the p value wrong...
 
 # Q4:
 model4 <- 
@@ -66,6 +67,14 @@ summary(model4)
 # Also wrong, try this:
 # The estimated expected change in MPG per half ton increase in weight 
 # for for a specific number of cylinders (4, 6, 8).
+
+# oops this should be it:
+# The estimated expected change in MPG per one ton increase in weight.
+# It's 2000lb is a ton
+
+# damn it should maybe:
+# The estimated expected change in MPG per one ton increase in weight 
+# for a specific number of cylinders (4, 6, 8).
 
 # Q5:
 x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
